7905fe764994cd6dcc5e2aa800c7ab7ddfa0689a
Emdadul Islam <bekon1700@gmail.com>
2022-09-29 17:01:14 +0600

    full done 2

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ function App() {
   }, [])
   return (
     <div>
-      <div className="grid grid-cols-12 border-b-2 border-gray-600">
+      <div className="grid grid-cols-12  border-b-2 border-gray-600">
         <div className="col-span-12 lg:col-span-9">
           <div className="w-11/12 mx-auto py-12">
             <Activities activities={activities} activityTime={activityTime} setActivityTime={setActivityTime}/>
           </div>
         </div>
-        <div className="col-span-12 lg:col-span-3 bg-[#e3f7ff] px-3 2xl:px-6 pt-14">
+        <div className="col-span-12 lg:col-span-3 bg-[#e3f7ff] px-3 2xl:px-6 pt-14 pb-12">
           <Finisher activityTime={activityTime} setActivityTime={setActivityTime}/>
         </div>
       </div>
--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -8,12 +8,12 @@ const Blog = () => {
             answer: `ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.`
         },
         {
-            id: 1,
+            id: 2,
             question: 'Difference between props and state',
             answer: `Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components. The this. setState property is used to update the state values in the component.`
         },
         {
-            id: 1,
+            id: 3,
             question: 'Usage of useEffect without calling api',
             answer: `The useEffect hook allows you to perform side effects in a functional component. There is a dependency array to control when the effect should run. It runs when the component is mounted and when it is re-rendered while a dependency of the useEffect has changed.`
         },
